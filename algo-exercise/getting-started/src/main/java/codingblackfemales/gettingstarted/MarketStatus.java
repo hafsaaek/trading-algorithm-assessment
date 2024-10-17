f16a0933f2c4538478e036e0a3ef01118edf1efa
Hafsa Kahiye <xhafsaa20@gmail.com>
2024-10-17 18:26:23 +0100

    added marketStatus as an interface to mock in unit tests

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MarketStatus.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MarketStatus.java
@@ -1,20 +1,10 @@
 package codingblackfemales.gettingstarted;
 
-import java.time.*;
+/* Market Status Interface to mock in tests and avoid mocking concrete implementation */
 
-public class MarketStatus {
+public interface MarketStatus {
 
-    private static final LocalTime MARKET_OPEN_TIME = LocalTime.of(8, 0, 0);
-    private static final LocalTime MARKET_CLOSE_TIME = LocalTime.of(16, 35, 0); // market close time is after close market auction window to allow our algo to secure a good ask/bid price
-    private static final ZoneId LONDON_TIME_ZONE = ZoneId.of("Europe/London");
+    boolean isMarketOpen();
 
-    public boolean isMarketOpen() {
-        ZonedDateTime timeNow = ZonedDateTime.now(LONDON_TIME_ZONE); // Define London time zone & the present time
-        LocalDate today = LocalDate.now(LONDON_TIME_ZONE); // Declare today's date according to London's time zone
-        ZonedDateTime marketOpenDateTime = ZonedDateTime.of(today, MARKET_OPEN_TIME, LONDON_TIME_ZONE);  // Declare market opening conditions
-        ZonedDateTime marketCloseDateTime = ZonedDateTime.of(today, MARKET_CLOSE_TIME, LONDON_TIME_ZONE); // Declare market closing conditions
 
-        // Deduce if the current time is before opening, after closing, or on a weekend - we will ignore holidays for now
-        return timeNow.isAfter(marketOpenDateTime) || timeNow.isBefore(marketCloseDateTime) || today.getDayOfWeek() != DayOfWeek.SATURDAY || today.getDayOfWeek() != DayOfWeek.SUNDAY; // Market is open at 8am and closed before 8am & after 4.35pm & on weekend
-    }
 }
